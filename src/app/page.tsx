7e2922196b40f2c7612488722eb2cc868661cfab
Samil Abud <samilabud@gmail.com>
2024-01-08 22:30:04 -0400

    Added responsive behavior to current weather container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-between h-full">
-      <div className="w-4/5 p-2">
-        <div className="w-full bg-zinc-500 opacity-35 flex items-center h-full flex-col rounded-xl">
-          <h2 className="font-mono text-3xl text-white bold">28</h2>
-          <h4 className="font-mono text-3xl text-white">Cloudy Day</h4>
+    <div className="flex justify-between h-full w-full min-w-96">
+      <div className="flex w-4/5 p-6">
+        <div className="w-full flex items-center h-full flex-col rounded-xl bg-opacity-80 bg-black pt-24">
+          <h2 className="font-mono text-6xl text-white bold">28°</h2>
+          <h4 className="font-mono text-3xl text-white mt-1 whitespace-nowrap p-3">
+            Cloudy Day
+          </h4>
         </div>
       </div>
       <div className="w-full bg-red-700  p-2">col2</div>
